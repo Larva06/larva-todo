d823667d10df7247fca9f224d2938b37870fc231
kendea1221 <kenshirotamata@gmail.com>
2025-03-26 08:44:22 +0900

    fix: 依頼した人はメンションではなくDisplayNameに変更

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -164,7 +164,7 @@ const slashCommand = {
         if (channel instanceof TextChannel) {
 
             const taskMessage = await channel.send({
-                content: format(messages.guild.task.title, assignee.toString(),interaction.user.toString()),
+                content: format(messages.guild.task.title, assignee.toString(),interaction.user.displayName.toString()),
                 embeds: [taskCheckEmbed]
             });
         
