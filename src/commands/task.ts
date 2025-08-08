9ac6da75281ad90fb160ed6ce4a827ec6dd95166
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2025-08-09 02:08:09 +0900

    fix: ESLintのエラーを修正 (#64)

--- src/commands/task.ts
+++ src/commands/task.ts
@@ -167,11 +167,7 @@ const slashCommand = {
 
         if (channel instanceof TextChannel) {
             const taskMessage = await channel.send({
-                content: format(
-                    messages.guild.task.title,
-                    assignee.toString(),
-                    interaction.user.displayName.toString()
-                ),
+                content: format(messages.guild.task.title, assignee.toString(), interaction.user.displayName),
                 embeds: [taskCheckEmbed]
             });
             const { resource } = interactionCallbackResponse;
